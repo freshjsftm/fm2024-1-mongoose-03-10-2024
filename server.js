74b82ff034763eebe9e9b085b6521d0ee09c90a0
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-10-10 11:41:15 +0300

    Update connect mongoose

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 const app = require('./app');
 
 mongoose
-.connect('mongodb://localhost:27017/fm2024mongoose')
+.connect('mongodb://172.17.0.2:27017/fm2024mongoose')
 .catch((err) => {
   console.log('Error in DB connection: ' + err);
   process.exit(1);
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3003;
 const server = http.createServer(app);
 
 server.listen(port, () => {
